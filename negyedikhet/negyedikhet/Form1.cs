1fa6458cf867df68aa1251790e43d27c6504c64b
toroklili <lili.torok@stud.uni-corvinus.hu>
2022-10-04 21:50:49 +0200

    xxx

--- negyedikhet/negyedikhet/Form1.cs
+++ negyedikhet/negyedikhet/Form1.cs
@@ -7,14 +7,105 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace negyedikhet
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> Flats;
+
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+            try
+            {
+                // Excel elindítása és az applikáció objektum betöltése
+                xlApp = new Excel.Application();
+
+                // Új munkafüzet
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+                // Új munkalap
+                xlSheet = xlWB.ActiveSheet;
+
+                // Tábla létrehozása
+                CreateTable(); // Ennek megírása a következő feladatrészben következik
+
+                // Control átadása a felhasználónak
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex) // Hibakezelés a beépített hibaüzenettel
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                // Hiba esetén az Excel applikáció bezárása automatikusan
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
+
+
         }
+         Excel.Application xlApp; // A Microsoft Excel alkalmazás
+                    Excel.Workbook xlWB; // A létrehozott munkafüzet
+                    Excel.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+
+        private void CreateTable()
+        {
+            string[] headers = new string[] 
+            {
+             "Kód",
+             "Eladó",
+             "Oldal",
+             "Kerület",
+             "Lift",
+             "Szobák száma",
+             "Alapterület (m2)",
+             "Ár (mFt)",
+             "Négyzetméter ár (Ft/m2)"
+            };
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, 1] = headers[0];
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+            //Egy foreach ciklussal menj végig a Flats lista sorain, és tölts fel a tömböt a megfelelő adatokkal.
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                values[counter, 4] = f.Elevator;
+                //A lift oszlopban a true és a false értékek helyett "Van" és "Nincs" jelenjen meg.
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "";
+                counter++;
+            }
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flats.ToList();
+            // A List<Flat> Flats = context.Flats.ToList() sor lemásolja a context.Flats táblát egy Flats nevű Flat típusú elemekből álló listába a memóriába. A listán végrehajtott LINQ műveletek már lokálisan kerülnek kiszámolásra a szerver terhelése nélkül. (LINQ to SQL vs. LINQ to objets)
+        }
+
+       
+
+ 
+
+
     }
 }
