f1ab1da16291ed60a9634f711235a96cf2438e06
toroklili <lili.torok@stud.uni-corvinus.hu>
2022-10-10 11:49:13 +0200

    adatmodell

---
+++ Olympics/Olympics/medálok/OlympicResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olympics.medálok
+{
+    public class OlympicResult
+    {
+        public int Year { get; set; }
+        public string Country { get; set; }
+        public  string[] Medals { get; set; }
+        public int Position { get; set; }
+
+
+    }
+}
