2f7f8a53d415979e83521457692043050a350136
q97yzb <q97yzb@SZA0025786>
2022-10-21 12:42:36 +0200

    7edik

--- hatodikhet/hatodikhet/Form1.cs
+++ hatodikhet/hatodikhet/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace hatodikhet
@@ -21,36 +22,45 @@ namespace hatodikhet
         public Form1()
         {
             InitializeComponent();
-            var mnbService = new MNBArfolyamServiceSoapClient();
 
+            RefreshData();
+
+        }
+
+        private void RefreshData()
+        {
+            Rates.Clear();
             Webszolg();
 
 
             dataGridView1.DataSource = Rates;
 
-            GetXML();
-            Adatmegjelenites();
 
+            Adatmegjelenites();
         }
-        private void Webszolg()
+
+        public void Webszolg()
         {
+          
             var mnbService = new MNBArfolyamServiceSoapClient();
 
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = (comboBox1.SelectedItem).ToString(),
+                startDate = (dateTimePicker1.Value).ToString(),
+                endDate = (dateTimePicker2.Value).ToString()
             };
 
             var response = mnbService.GetExchangeRates(request);
 
             var result = response.GetExchangeRatesResult;
+            GetXML(result);
         }
        
 
-        private void GetXML()
+        private void GetXML(string result)
         {
+
             var xml = new XmlDocument();
             xml.LoadXml(result);
             foreach (XmlElement element in xml.DocumentElement)
@@ -76,9 +86,36 @@ namespace hatodikhet
 
         private void Adatmegjelenites()
         {
+            chartRateData.DataSource = Rates;
 
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
 
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
 
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
     }
 }
