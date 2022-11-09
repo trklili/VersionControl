01c72b1233848a2d9fa891e62e028c96b50c73d5
q97yzb <q97yzb@SZA0025786>
2022-11-09 18:03:33 +0100

    Portfolió tárolása memóriában

--- hetedik/hetedik/Form1.cs
+++ hetedik/hetedik/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using hetedik.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,12 +15,29 @@ namespace hetedik
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
 
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            /*PortfolioItem p = new PortfolioItem();
+            p.Index = "OTP";
+            p.Volume = 10;
+            Portfolio.Add(p);
+            */
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
