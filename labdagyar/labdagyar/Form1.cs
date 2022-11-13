184fca9c331b51dee0d1291f3449937cad4b4713
toroklili <lili.torok@stud.uni-corvinus.hu>
2022-11-13 17:47:33 +0100

    Absztrakció felhasználása a futószalagban

--- labdagyar/labdagyar/Form1.cs
+++ labdagyar/labdagyar/Form1.cs
@@ -1,4 +1,5 @@
-﻿using labdagyar.Entities;
+﻿using labdagyar.Abstractions;
+using labdagyar.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,10 +14,10 @@ namespace labdagyar
 {
     public partial class Form1 : Form
     {
-        private List<Ball> _balls = new List<Ball>();
+        private List<Toy> _toys = new List<Toy>();
 
-        private BallFactory _factory;
-        public BallFactory Factory
+        private IToyFactory _factory;
+        public IToyFactory Factory
         {
             get { return _factory; }
             set { _factory = value; }
@@ -25,7 +26,7 @@ namespace labdagyar
         public Form1()
         {
             InitializeComponent();
-            Factory = new BallFactory();
+            Factory = new CarFactory();
         }
 
         
@@ -34,28 +35,28 @@ namespace labdagyar
 
         private void createTimer_Tick_1(object sender, EventArgs e)
         {
-            var ball = Factory.CreateNew();
-            _balls.Add(ball);
-            ball.Left = -ball.Width;
-            mainPanel.Controls.Add(ball);
+            var toy = Factory.CreateNew();
+            _toys.Add(toy);
+            toy.Left = -toy.Width;
+            mainPanel.Controls.Add(toy);
 
         }
 
         private void conveyorTimer_Tick_1(object sender, EventArgs e)
         {
              var maxPosition = 0;
-                        foreach (var ball in _balls)
+                        foreach (var toy in _toys)
                         {
-                            ball.MoveToy();
-                            if (ball.Left > maxPosition)
-                                maxPosition = ball.Left;
+                            toy.MoveToy();
+                            if (toy.Left > maxPosition)
+                                maxPosition = toy.Left;
                         }
 
                         if (maxPosition > 1000)
                         {
-                            var oldestBall = _balls[0];
+                            var oldestBall = _toys[0];
                             mainPanel.Controls.Remove(oldestBall);
-                            _balls.Remove(oldestBall);
+                            _toys.Remove(oldestBall);
                         }
         }
     }
