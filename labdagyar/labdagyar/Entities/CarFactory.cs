724f8eb8be8e923ec7eb9035a8193e32ac899854
toroklili <lili.torok@stud.uni-corvinus.hu>
2022-11-13 17:43:10 +0100

    interfész létrehozás

--- labdagyar/labdagyar/Entities/CarFactory.cs
+++ labdagyar/labdagyar/Entities/CarFactory.cs
@@ -7,13 +7,11 @@ using System.Threading.Tasks;
 
 namespace labdagyar.Entities
 {
-    public class BallFactory : IToyFactory
+    public class CarFactory :IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
-
-       
     }
 }
