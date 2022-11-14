0997be82bde258f785ed25acf2a99c503d3717f3
toroklili <lili.torok@stud.uni-corvinus.hu>
2022-11-14 10:47:53 +0100

    színválasztó gomb

--- labdagyar/labdagyar/Form1.Designer.cs
+++ labdagyar/labdagyar/Form1.Designer.cs
@@ -30,16 +30,55 @@
         {
             this.components = new System.ComponentModel.Container();
             this.mainPanel = new System.Windows.Forms.Panel();
+            this.ball_btn = new System.Windows.Forms.Button();
+            this.car_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.createTimer = new System.Windows.Forms.Timer(this.components);
             this.conveyorTimer = new System.Windows.Forms.Timer(this.components);
+            this.color_btn = new System.Windows.Forms.Button();
+            this.mainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainPanel
             // 
-            this.mainPanel.Location = new System.Drawing.Point(-2, -3);
+            this.mainPanel.Controls.Add(this.color_btn);
+            this.mainPanel.Controls.Add(this.ball_btn);
+            this.mainPanel.Controls.Add(this.car_btn);
+            this.mainPanel.Controls.Add(this.label1);
+            this.mainPanel.Location = new System.Drawing.Point(-3, -4);
+            this.mainPanel.Margin = new System.Windows.Forms.Padding(4);
             this.mainPanel.Name = "mainPanel";
-            this.mainPanel.Size = new System.Drawing.Size(803, 454);
+            this.mainPanel.Size = new System.Drawing.Size(1070, 558);
             this.mainPanel.TabIndex = 0;
+            // 
+            // ball_btn
+            // 
+            this.ball_btn.Location = new System.Drawing.Point(32, 208);
+            this.ball_btn.Name = "ball_btn";
+            this.ball_btn.Size = new System.Drawing.Size(75, 23);
+            this.ball_btn.TabIndex = 2;
+            this.ball_btn.Text = "BALL";
+            this.ball_btn.UseVisualStyleBackColor = true;
+            this.ball_btn.Click += new System.EventHandler(this.ball_btn_Click);
+            // 
+            // car_btn
+            // 
+            this.car_btn.Location = new System.Drawing.Point(32, 165);
+            this.car_btn.Name = "car_btn";
+            this.car_btn.Size = new System.Drawing.Size(75, 23);
+            this.car_btn.TabIndex = 1;
+            this.car_btn.Text = "CAR";
+            this.car_btn.UseVisualStyleBackColor = true;
+            this.car_btn.Click += new System.EventHandler(this.car_btn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Coming next:";
             // 
             // createTimer
             // 
@@ -53,14 +92,27 @@
             this.conveyorTimer.Interval = 10;
             this.conveyorTimer.Tick += new System.EventHandler(this.conveyorTimer_Tick_1);
             // 
+            // color_btn
+            // 
+            this.color_btn.BackColor = System.Drawing.Color.Blue;
+            this.color_btn.Location = new System.Drawing.Point(32, 250);
+            this.color_btn.Name = "color_btn";
+            this.color_btn.Size = new System.Drawing.Size(75, 23);
+            this.color_btn.TabIndex = 3;
+            this.color_btn.UseVisualStyleBackColor = false;
+            this.color_btn.Click += new System.EventHandler(this.color_btn_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.mainPanel);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.mainPanel.ResumeLayout(false);
+            this.mainPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -70,6 +122,10 @@
         private System.Windows.Forms.Panel mainPanel;
         private System.Windows.Forms.Timer createTimer;
         private System.Windows.Forms.Timer conveyorTimer;
+        private System.Windows.Forms.Button ball_btn;
+        private System.Windows.Forms.Button car_btn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button color_btn;
     }
 }
 
--- labdagyar/labdagyar/Form1.cs
+++ labdagyar/labdagyar/Form1.cs
@@ -16,6 +16,8 @@ namespace labdagyar
     {
         private List<Toy> _toys = new List<Toy>();
 
+        private Toy _nextToy;
+
         private IToyFactory _factory;
         public IToyFactory Factory
         {
@@ -58,6 +60,37 @@ namespace labdagyar
                             mainPanel.Controls.Remove(oldestBall);
                             _toys.Remove(oldestBall);
                         }
+        }
+
+        private void car_btn_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void ball_btn_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
+        }
+
+        private void DisplayNext()
+        {
+            if (_nextToy != null)
+                Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
+        }
+
+        private void color_btn_Click(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            var colorPicker = new ColorDialog();
+
+            colorPicker.Color = button.BackColor;
+            if (colorPicker.ShowDialog() != DialogResult.OK)
+                return;
+            button.BackColor = colorPicker.Color;
         }
     }
 }
