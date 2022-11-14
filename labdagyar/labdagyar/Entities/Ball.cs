ad906b8d99d2a0bf2d8fd98a229c7c7ad3c2da84
toroklili <lili.torok@stud.uni-corvinus.hu>
2022-11-14 10:54:29 +0100

    .

--- labdagyar/labdagyar/Entities/Ball.cs
+++ labdagyar/labdagyar/Entities/Ball.cs
@@ -19,7 +19,7 @@ namespace labdagyar.Entities
         }
         protected override void DrawImage(Graphics g)
         {
-            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+            g.FillEllipse(BallColor, 0, 0, Width, Height);
         }
     }
 }
