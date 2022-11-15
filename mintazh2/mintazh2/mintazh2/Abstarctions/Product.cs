c0201377eb4908749e113440a4b3d67f0df572bd
toroklili <lili.torok@stud.uni-corvinus.hu>
2022-11-15 21:38:38 +0100

    Product absztrakt osztály

--- mintazh2/mintazh2/mintazh2/Abstarctions/Product.cs
+++ mintazh2/mintazh2/mintazh2/Abstarctions/Product.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
 
 namespace mintazh2.Abstarctions
 {
-    internal class Product
+    public abstract class Product: Button
     {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set 
+            {
+                _title = value;
+                Text = Title;
+            }
+        }
+
+        private string _calories;
+        public string Calories
+        {
+            get { return _calories; }
+            set 
+            { 
+                _calories = value;
+                Display();
+            }
+        }
+
+        //visszatérés nélküli absztrakt fgv
+        protected abstract void Display();
+
+        //konstruktor
+        public Product()
+        {
+            Width = 150;
+            Height = 50;
+        }
     }
 }
