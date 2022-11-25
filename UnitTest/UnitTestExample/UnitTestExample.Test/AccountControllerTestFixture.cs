4805f6469d2e8baa519f7ac0435c310929d22af8
q97yzb <q97yzb@SZA0025786>
2022-11-25 13:02:52 +0100

    hibak kijavítv regiusterbe

--- UnitTest/UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTest/UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -74,14 +74,14 @@ namespace UnitTestExample.Test
         }
 
             [
-    Test,
-    TestCase("irf@uni-corvinus", "Abcd1234"),
-    TestCase("irf.uni-corvinus.hu", "Abcd1234"),
-    TestCase("irf@uni-corvinus.hu", "abcd1234"),
-    TestCase("irf@uni-corvinus.hu", "ABCD1234"),
-    TestCase("irf@uni-corvinus.hu", "abcdABCD"),
-    TestCase("irf@uni-corvinus.hu", "Ab1234"),
-]
+                Test,
+                TestCase("irf@uni-corvinus", "Abcd1234"),
+                TestCase("irf.uni-corvinus.hu", "Abcd1234"),
+                TestCase("irf@uni-corvinus.hu", "abcd1234"),
+                TestCase("irf@uni-corvinus.hu", "ABCD1234"),
+                TestCase("irf@uni-corvinus.hu", "abcdABCD"),
+                TestCase("irf@uni-corvinus.hu", "Ab1234"),
+            ]
         public void TestRegisterValidateException(string email, string password)
         {
             // Arrange
--- UnitTest/UnitTestExample/UnitTestExample/Controllers/AccountController.cs
+++ UnitTest/UnitTestExample/UnitTestExample/Controllers/AccountController.cs
@@ -25,7 +25,7 @@ namespace UnitTestExample.Controllers
             if(!ValidateEmail(email))
                 throw new ValidationException(
                     "A megadott e-mail cím nem megfelelő!");
-            if(!ValidateEmail(email))
+            if(!ValidatePassword(password))
                 throw new ValidationException(
                     "A megadottt jelszó nem megfelelő!\n" +
                     "A jelszó legalább 8 karakter hosszú kell legyen, csak az angol ABC betűiből és számokból állhat, és tartalmaznia kell legalább egy kisbetűt, egy nagybetűt és egy számot.");
@@ -33,7 +33,8 @@ namespace UnitTestExample.Controllers
             var account = new Account()
             {
                 Email = email,
-                Password = password
+                Password = password,
+                ID= Guid.NewGuid(),
             };
 
             var newAccount = AccountManager.CreateAccount(account);
