b2924ebd7aea48053af58b009ae8970c7178beb7
q97yzb <q97yzb@SZA0025786>
2022-12-09 12:19:29 +0100

    elso

--- 10het/10het/Form1.cs
+++ 10het/10het/Form1.cs
@@ -12,9 +12,36 @@ namespace _10het
 {
     public partial class Form1 : Form
     {
+        GameController gc = new GameController();
+        GameArea ga;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
+        Brain winnerBrain = null;
         public Form1()
         {
             InitializeComponent();
+            button1.Visible = false;
+
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
+
+            /* gc.AddPlayer();
+             gc.Start(true);
+            */
+
+
+            gc.GameOver += Gc_GameOver;
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
+
         }
     }
 }
